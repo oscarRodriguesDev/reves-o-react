6c290261f6e051a8e63d5d709827c34125d3492f
Oscar <oskharm12@gmail.com>
2024-09-25 22:10:17 -0300

    first commit

---
+++ fundamentos/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { PrimeiroComponente } from './components/firsComponents';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <PrimeiroComponente/>
+    </div>
+  );
+}
+
+export default App;
---
+++ fundamentos/src/components/firsComponents.js
@@ -0,0 +1,7 @@
+export const PrimeiroComponente = () =>{
+return(
+    <>
+    <h1>Meu primeiro componente</h1>
+    </>
+)
+}
