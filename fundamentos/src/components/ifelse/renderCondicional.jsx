a8efcecb223e8e621b2c4efd02618d3b1b24ffc7
Oscar <oskharm12@gmail.com>
2024-10-02 09:10:56 -0300

    subindo ultimas atualizações

--- fundamentos/src/components/ifelse/renderCondicional.jsx
+++ fundamentos/src/components/ifelse/renderCondicional.jsx
@@ -3,10 +3,19 @@ import {useState} from 'react'
 export const RenderCondicional= ()=>{
 
     const [x] = useState(false)
+
+    const [name] = useState('Joãod')
     return(
         <div>
          {x && <h1>Valor é true</h1>}
          {!x && <h1>Valor é false</h1>}
+
+         {/* vamos exibir algo se op name for joão ou não */}
+
+          <div>
+            <h1>If ternario</h1>
+            {name ==='João' ? (<p>João</p>):(<p>Nome diferente de joão</p>) }
+          </div>
         </div>
     )
 }
