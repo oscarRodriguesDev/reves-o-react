1106b5c6351dce4802c55c105402f02db13c5e32
Dikma Digital2 <dikma.digital2@grupodikma.com.br>
2024-10-09 15:31:43 -0300

    projeto added in course module css

---
+++ css/css_react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='title'>Curso de react module css</h1>
+       <h1>Trabalhando com react css</h1>
+      {/*  */}
+    </div>
+  );
+}
+
+export default App;
