536ae2e22ff31615ebcd8151a41806baf3085fbd
Dikma Digital2 <dikma.digital2@grupodikma.com.br>
2024-10-22 17:46:27 -0300

    requisições acontecendo

--- reqhttp/httpreact/src/App.js
+++ reqhttp/httpreact/src/App.js
@@ -1,12 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState,useEffect} from 'react'
+
 
 function App() {
+  const [products,setProducts]=  useState([]);
+  const url = 'http://localhost:3000/products'
+
+  
+  async function recData(){
+   
+    try{
+      const data = await fetch(url)
+      const res = await data.json()
+      return res
+  
+    }catch(error){
+      console.log('erro')
+    }
+  }
+
+useEffect(() => {
+  setProducts(recData());
+  if(products){
+
+    setProducts(recData());
+    console.log(products)
+  }
+
+},[])
+
+
   return (
     <div className="App">
      <h1>Requisições http</h1>
+     
     </div>
   );
 }
 
 export default App;
+
+//inicializar o servidor
