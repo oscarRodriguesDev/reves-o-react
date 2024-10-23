ab3a7d5c81cf2ace4fb9c2cadd197fe4b91e40ad
Dikma Digital2 <dikma.digital2@grupodikma.com.br>
2024-10-23 17:37:00 -0300

    alteração aula requ

--- reqhttp/httpreact/src/App.js
+++ reqhttp/httpreact/src/App.js
@@ -1,44 +1,51 @@
-
 import './App.css';
-import {useState,useEffect} from 'react'
-
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [products,setProducts]=  useState([]);
-  const url = 'http://localhost:3000/products'
+  const [products, setProducts] = useState([]);
+  const url = 'http://localhost:3000/products';
 
-  
-  async function recData(){
-   
-    try{
-      const data = await fetch(url)
-      const res = await data.json()
-      return res
-  
-    }catch(error){
-      console.log('erro')
+  // Função para buscar os dados
+  async function recData() {
+    try {
+      const data = await fetch(url);
+      const res = await data.json();
+      return res;
+    } catch (error) {
+      console.log('Erro ao buscar dados:', error);
     }
   }
 
-useEffect(() => {
-  setProducts(recData());
-  if(products){
+  useEffect(() => {
+    async function fetchProducts() {
+      const result = await recData();
+      if (result) {
+        setProducts(result); // Atualiza o estado com os produtos
+      }
+      console.log(result)
+    }
 
-    setProducts(recData());
-    console.log(products)
-  }
-
-},[])
-
+    fetchProducts(); // Chama a função ao montar o componente
+  }, []); // Array vazio para que o efeito seja executado apenas uma vez
 
   return (
     <div className="App">
-     <h1>Requisições http</h1>
-     
+      <h1>Requisições HTTP</h1>
+
+      {products.map((product) => (
+        <ul key={product.id}>
+      
+          <li>{product.name }</li>
+
+        </ul>
+
+
+      ))}
+
+
+
     </div>
   );
 }
 
 export default App;
-
-//inicializar o servidor
